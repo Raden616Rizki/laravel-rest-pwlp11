db0bfc748541c2152712d54ae8565116566e220e
Raden616Rizki <rizkiraden616@gmail.com>
2023-05-17 21:27:12 +0700

    2# Commit Praktikum 2

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mahasiswas'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamp = false;
+    protected $primaryKey = 'nim'; // Memanggil isi DB dengan Primary Key
+    public $incrementing = false;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas_id',
+        'jurusan',
+        'no_hp',
+    ];
+}
